6fd6d8f764285ca2fccfdae38f89afb8206250a2
edjacks <edjacks@users.noreply.github.com>
2018-10-29 11:40:07 -0400

    Update license-check.sh
    Added check for environment variable for SNMP RO string.  Added output for UDI of CSR1000V

--- license-check.sh
+++ license-check.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# bash script to pull active license feature and expiration date from IOS-XE routers.
-#
-# requirements:
-#	* snmpwalk in path
-#	* environment variable SNMPRO set to the RO community string to use
-#	
-# usage:
-#	./license_check.sh [FQDN|IP]
-#
-# sample output:
-#
-#	$ ~/bin/license-check.sh 192.168.0.1
-#	192.168.0.1,appx_1G,2018-11-05
-#	$
-#
+# is SNMPRO set?
+if [ -z "$SNMPRO" ]; then
+	echo
+	echo "Please set environment variable SNMPRO."
+	echo "e.g.: export SNMPRO=CommunityString"
+	echo
+	exit
+fi
+
 inuse=".1.3.6.1.4.1.9.9.543.1.2.3.1.14.1.1"
 feature=".1.3.6.1.4.1.9.9.543.1.2.3.1.3.1.1"
 enddate=".1.3.6.1.4.1.9.9.543.1.2.3.1.16.1.1"
+udi=".1.3.6.1.2.1.47.1.1.1.1.11.1"
 
 # is site ${1} reachable?
 if ping -c 1 -w 5 ${1} &>/dev/null; then
@@ -36,11 +31,12 @@ if ping -c 1 -w 5 ${1} &>/dev/null; then
 	hex_day=$(echo ${license_hex} | cut -d" " -f4)
 	# convert to YYYY-MM-DD
 	license_date=$(printf "%04d-%02d-%02d" $(( 16#${hex_year} )) $(( 16#${hex_month} )) $(( 16#${hex_day} )))
+	udi_info=$(snmpwalk -v 2c -c ${SNMPRO} ${1} ${udi} | cut -d" " -f4 | cut -d\" -f2)
 
 	#print it out
-	echo "${1},${license_feature},${license_date}"
+	echo "${1},${udi_info},${license_feature},${license_date}"
 
 
 else
-	echo ${1},NOT reachable,1901-01-01
+	echo ${1},N/A,NOT reachable,1901-01-01
 fi
